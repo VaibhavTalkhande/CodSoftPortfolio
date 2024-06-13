09b9f87d87dba965d4afdf03b11da18dc1ae5944
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2024-06-13 19:02:35 +0530

    final

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 
-        // JavaScript to add smooth scrolling for navigation
-        document.querySelectorAll('.navbar span a').forEach(anchor => {
-            anchor.addEventListener('click', function(e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
-         document.addEventListener("DOMContentLoaded", function() {
-            const openDialogButtons = document.querySelectorAll('.open-dialog');
-            const closeDialogButtons = document.querySelectorAll('.close-dialog');
-        
-            openDialogButtons.forEach(button => {
-                button.addEventListener('click', function() {
-                    const dialogId = this.getAttribute('data-dialog');
-                    const dialog = document.getElementById(dialogId);
-                    if (dialog) {
-                        dialog.showModal();
-                    }
-                });
-            });
-        
-            closeDialogButtons.forEach(button => {
-                button.addEventListener('click', function() {
-                    const dialog = this.closest('dialog');
-                    if (dialog) {
-                        dialog.close();
-                    }
-                });
-            });
-        }); 
-        
+document.querySelectorAll(".navbar span a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const openDialogButtons = document.querySelectorAll(".open-dialog");
+  const closeDialogButtons = document.querySelectorAll(".close-dialog");
+
+  openDialogButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const dialogId = this.getAttribute("data-dialog");
+      const dialog = document.getElementById(dialogId);
+      if (dialog) {
+        dialog.showModal();
+      }
+    });
+  });
+
+  closeDialogButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const dialog = this.closest("dialog");
+      if (dialog) {
+        dialog.close();
+      }
+    });
+  });
+});
+
+function clearform() {
+  document.getElementById("contact-form").reset();
+}
